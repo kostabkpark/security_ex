96a301635f646545b56612e26a8898a32413b26c
kostabkpark <kostabkpark@gmail.com>
2025-06-04 10:29:42 +0900

    login process - mapper 수정

--- src/main/java/com/example/security_ex/repository/UsersMapperRepository.java
+++ src/main/java/com/example/security_ex/repository/UsersMapperRepository.java
@@ -8,6 +8,6 @@ import org.apache.ibatis.annotations.Select;
 public interface UsersMapperRepository {
   // 사용자 이름으로 로그인 사용자 정보를 조회하는 기능 추가
 
-  @Select("select username, password, authority, displayname, phoneNumber from users where username=:username")
+  @Select("select username, password, authority, displayname, phoneNumber from users where username= #{username}")
   public Users findByUsername(String username);
 }
--- src/main/java/com/example/security_ex/service/LoginUserDetailsServiceImpl.java
+++ src/main/java/com/example/security_ex/service/LoginUserDetailsServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.security_ex.domain.LoginUser;
 import com.example.security_ex.domain.Users;
 import com.example.security_ex.repository.UsersMapperRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,17 +16,21 @@ import java.util.Collection;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class LoginUserDetailsServiceImpl implements UserDetailsService {
   private final UsersMapperRepository usersMapperRepository;
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     // 인증 사용자 정보 테이블에서 로그인시 입력한 username 으로 사용자 정보 가져오기
+    log.info("loadUserByUsername 1 ===> {}", username);
     Users user = usersMapperRepository.findByUsername(username);
     // 사용자 정보가 없으면 null (익셉션), 있으면 LoginUser 생성해서 반환
     if(user == null) {
+      log.info("loadUserByUsername 2 ===> {}", user);
       return null;
     } else {
+      log.info("loadUserByUsername 3 ===> {}", user.getUsername());
       return User.builder()
           .username(user.getUsername())
           .password(user.getPassword())
